37b593e506c1e8047519195a54261599aa49ecd0
AlexandraLukacs <lszandika15@gmail.com>
2024-11-05 15:55:56 +0100

    lekerdezes

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    // egyéb lekérdezések
+    public function booksWithCopies(){
+        //$book = Auth::book();	//bejelentkezett felhasználó
+        // copies: függvény neve
+        return Book::with('copies')
+        //->where('book_id','=',$book->id)
+        ->get();
+    }
+}
